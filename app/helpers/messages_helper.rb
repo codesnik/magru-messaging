d021132fcce84216a8c7823d15c98fb934bfa531
codesnik <aronaxis@gmail.com>
2010-02-25 07:01:20 +0300

    message thread view, message_helpers

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,23 @@
 module MessagesHelper
+  def message_recipient_options message
+    options_from_collection_for_select( User.all - [current_user], :id, :name, message.recipient_id)
+  end
+
+  def message_from_to message
+    h(message.sender.try(:name)) + ' &rArr; '.html_safe + h(message.recipient.try(:name))
+  end
+
+  def message_unread? message
+    (current_user != message.sender) && message.unread?
+  end
+
+  def message_unread_mark message
+    if message_unread?(message)
+      '(NEW!!)'
+    end
+  end
+
+  def message_timestamp message
+    time_ago_in_words message.created_at
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,27 @@ class Message < ActiveRecord::Base
   belongs_to :parent, :class_name => "Message"
   belongs_to :sender, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
+  has_many :messages, :foreign_key => :parent_id
+
+  default_scope order('created_at desc')
+  scope :threads, where(:parent_id => nil)
+  scope :unread, where(:unread => true)
+  scope :with_user, lambda { |user|
+    where(["recipient_id = ? or sender_id = ?", user.id, user.id])
+  }
+
+  def thread_starter?
+    !parent_id
+  end
+
+  def thread
+    parent || self
+  end
+
+  def full_thread
+    thread.messages + [thread]
+  end
+
+  delegate :subject, :to => :thread, :prefix => :thread
+
 end
